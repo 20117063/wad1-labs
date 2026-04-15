adb9f358399a6b034ac4f58b1de04466f7d26bac
20117063 <20117063@setu.ie>
2026-04-15 14:41:51 +0100

    Added custom helpers

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -19,15 +19,21 @@ const dashboard = {
     
     response.render('dashboard', viewData);
   },
+
   addPlaylist(request, response) {
-    const newPlayList = {
+    const timestamp = new Date();
+    
+    const newPlaylist = {
       id: uuidv4(),
       title: request.body.title,
-      songs: [],
+      rating: parseInt(request.body.rating),
+	  date: timestamp,
+      songs: []
     };
-    playlistStore.addPlaylist(newPlayList);
+    playlistStore.addPlaylist(newPlaylist);
     response.redirect('/dashboard');
 },
+
 deletePlaylist(request, response) {
     const playlistId = request.params.id;
     logger.debug(`Deleting Playlist ${playlistId}`);
--- server.js
+++ server.js
@@ -1,48 +1,47 @@
 'use strict';
-import bodyParser from "body-parser";
 
 import express from 'express';
-import routes from './routes.js'; 
 import logger from "./utils/logger.js";
+import routes from './routes.js'; 
 import { create } from 'express-handlebars';
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
- app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false, }));
+
+const handlebars = create({
+  extname: '.hbs', 
+    helpers: {
+      uppercase: (inputString) => {
+        return inputString.toUpperCase();
+      },
+       formatDate: (date) => {
+    let dateCreated = new Date(date);
+    let options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+    };
+    return `${dateCreated.toLocaleDateString("en-IE", options)}`;
+},
+     highlightPopular: (rating) => {
+   let message = rating >= 4 ? "Popular with listeners!" :  "";
+   return message;
+},
+
+    },
+});
+	  
+  
 
 
-const handlebars = create({extname: '.hbs'});
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
-
 app.use("/", routes);
 
-app.get('/about', (request, response) => response.send('About the Playlist app!'));   
-app.get('/', (request, response) => response.send('Welcome to the Playlist app!'));
-app.get("/dashboard", (request, response) => {
-  const playlist = [
-    {
-      id: 1,
-      title: "Piano Sonata No. 3",
-      artist: "Beethoven"
-    },
-    {
-      id: 2,
-      title: "Piano Sonata No. 7",
-      artist: "Beethoven"
-    },
-    {
-      id: 3,
-      title: "Piano Sonata No. 10",
-      artist: "Beethoven"
-    }
-  ];
-  response.json(playlist);
-});
-app.get('/error', (request, response) => response.status(404).end('Page not found.'));
-
-app.listen(port, () => logger.info(`Your app is listening on port ${port}!`));
+app.listen(port, () => logger.info(`Your app is listening on port ${port}`));
